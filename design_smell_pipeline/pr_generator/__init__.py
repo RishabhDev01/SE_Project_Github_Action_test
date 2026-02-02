aa2a845d21c1e5f2356db96e178ad7b0c08e936e
Rishabhsingh1109 <rishabhsingh11092002@gmail.com>
2026-02-02 15:14:32 +0530

    Fix RefactoringResult import error

--- design_smell_pipeline/pr_generator/__init__.py
+++ design_smell_pipeline/pr_generator/__init__.py
@@ -4,6 +4,7 @@ PR Generator Module - Git operations and GitHub Pull Request creation
 
 from .git_operations import GitOperations
 from .github_api import GitHubAPI
-from .pr_description import PRDescriptionGenerator
+from .pr_description import PRDescriptionGenerator, RefactoringResult
 
-__all__ = ['GitOperations', 'GitHubAPI', 'PRDescriptionGenerator']
+__all__ = ['GitOperations', 'GitHubAPI', 'PRDescriptionGenerator', 'RefactoringResult']
+
