9689631618d5ced420d1ca1a113b3f329db30766
runner <runner@runnervmkj6or>
2026-02-03 09:44:31 +0000

    refactor: Automated design smell fixes - 2026-02-03 09:44

--- app/src/main/java/org/apache/roller/weblogger/business/themes/ThemeMetadataParser.java
+++ app/src/main/java/org/apache/roller/weblogger/business/themes/ThemeMetadataParser.java
@@ -31,7 +31,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-
 /**
  * The parser for theme xml descriptors.
  *
@@ -44,19 +43,66 @@ public class ThemeMetadataParser {
      * set of Java objects.
      **/
     public ThemeMetadata unmarshall(InputStream instream)
-        throws ThemeParsingException, IOException, JDOMException {
-        
-        if(instream == null) {
+            throws ThemeParsingException, IOException, JDOMException {
+
+        if (instream == null) {
             throw new IOException("InputStream is null!");
         }
-        
+
         ThemeMetadata theme = new ThemeMetadata();
-        
-        SAXBuilder builder = new SAXBuilder();
-        Document doc = builder.build(instream);
-        
-        // start at root and get theme id, name, description and author
+
+        Document doc = parseDocument(instream);
         Element root = doc.getRootElement();
+
+        theme = parseThemeMetadata(root, theme);
+
+        // now grab the preview image path
+        Element previewImage = root.getChild("preview-image");
+        if (previewImage != null) {
+            theme.setPreviewImage(previewImage.getAttributeValue("path"));
+        } else {
+            throw new ThemeParsingException("No preview image specified");
+        }
+
+        // grab the stylesheet if it exists
+        Element stylesheet = root.getChild("stylesheet");
+        if (stylesheet != null) {
+            theme.setStylesheet(elementToStylesheet(stylesheet));
+        }
+
+        // now grab the static resources
+        List<Element> resources = root.getChildren("resource");
+        for (Element resource : resources) {
+            theme.addResource(resource.getAttributeValue("path"));
+        }
+
+        // now grab the templates
+        boolean weblogActionTemplate = false;
+        List<Element> templates = root.getChildren("template");
+        for (Element template : templates) {
+            ThemeMetadataTemplate tmpl = elementToTemplateMetadata(template);
+            theme.addTemplate(tmpl);
+
+            if (ComponentType.WEBLOG.equals(tmpl.getAction())) {
+                weblogActionTemplate = true;
+            }
+        }
+
+        // make sure all required elements are present and values are valid
+        // check that there is a template with action='weblog'
+        if (!weblogActionTemplate) {
+            throw new ThemeParsingException("did not find a template of action = 'weblog'");
+        }
+
+        return theme;
+    }
+
+    private Document parseDocument(InputStream instream) throws JDOMException, IOException {
+        SAXBuilder builder = new SAXBuilder();
+        return builder.build(instream);
+    }
+
+    private ThemeMetadata parseThemeMetadata(Element root, ThemeMetadata theme) throws ThemeParsingException {
         theme.setId(root.getChildText("id"));
         theme.setName(root.getChildText("name"));
         theme.setDescription(root.getChildText("description"));
@@ -66,63 +112,22 @@ public class ThemeMetadataParser {
         theme.setDualTheme("true".equalsIgnoreCase(root.getChildText("dualTheme")));
 
         // if either id or name is null then throw a parsing exception
-        if(StringUtils.isEmpty(theme.getId()) || StringUtils.isEmpty(theme.getName())) {
+        if (StringUtils.isEmpty(theme.getId()) || StringUtils.isEmpty(theme.getName())) {
             throw new ThemeParsingException("'id' and 'name' are required theme elements");
         }
-        
-        // now grab the preview image path
-        Element previewImage = root.getChild("preview-image");
-        if (previewImage != null) {
-            theme.setPreviewImage(previewImage.getAttributeValue("path"));
-        } else {
-            throw new ThemeParsingException("No preview image specified");
-        }
-        
-        // grab the stylesheet if it exists
-        Element stylesheet = root.getChild("stylesheet");
-        if (stylesheet != null) {
-            theme.setStylesheet(elementToStylesheet(stylesheet));
 
-        }
-        
-        // now grab the static resources
-        List<Element> resources = root.getChildren("resource");
-        for (Element resource : resources) {
-            theme.addResource(resource.getAttributeValue("path"));
-        }
-        
-        // now grab the templates
-        boolean weblogActionTemplate = false;
-        List<Element> templates = root.getChildren("template");
-        for (Element template : templates) {
-
-            ThemeMetadataTemplate tmpl = elementToTemplateMetadata(template);
-            theme.addTemplate(tmpl);
-
-            if(ComponentType.WEBLOG.equals(tmpl.getAction())) {
-                weblogActionTemplate = true;
-            }
-        }
-        
-        // make sure all required elements are present and values are valid
-        // check that there is a template with action='weblog'
-        if(!weblogActionTemplate) {
-            throw new ThemeParsingException("did not find a template of action = 'weblog'");
-        }
-        
         return theme;
     }
-    
-    
-	private ThemeMetadataTemplate elementToTemplateMetadata(Element element)
-			throws ThemeParsingException {
 
-		ThemeMetadataTemplate template = new ThemeMetadataTemplate();
+    private ThemeMetadataTemplate elementToTemplateMetadata(Element element)
+            throws ThemeParsingException {
 
-		template.setName(element.getChildText("name"));
-		template.setDescription(element.getChildText("description"));
-		template.setLink(element.getChildText("link"));
-		template.setContentType(element.getChildText("contentType"));
+        ThemeMetadataTemplate template = new ThemeMetadataTemplate();
+
+        template.setName(element.getChildText("name"));
+        template.setDescription(element.getChildText("description"));
+        template.setLink(element.getChildText("link"));
+        template.setContentType(element.getChildText("contentType"));
         String actionString = element.getAttributeValue("action");
         if (StringUtils.isEmpty(actionString)) {
             throw new ThemeParsingException("Template must contain an 'action' element");
@@ -134,63 +139,66 @@ public class ThemeMetadataParser {
             }
         }
 
-		//parsing template code segment
-		List<Element> renditionList = element.getChildren("rendition");
+        // parsing template code segment
+        List<Element> renditionList = element.getChildren("rendition");
+        for (Element renditionElement : renditionList) {
+            ThemeMetadataTemplateRendition rendition = parseRendition(renditionElement);
+            template.addTemplateRendition(rendition);
+        }
 
-		for (Element renditionElement : renditionList) {
-			ThemeMetadataTemplateRendition rendition = new ThemeMetadataTemplateRendition();
-            String renditionValue = renditionElement.getAttributeValue("type");
-            if (renditionValue != null) {
-                try {
-                    rendition.setType(RenditionType.valueOf(renditionValue.toUpperCase()));
-                } catch (IllegalArgumentException e) {
-                    throw new ThemeParsingException("Invalid rendition type " + renditionValue + " found.");
-                }
-            } else {
-                // default to standard if type not provided, as most templates are single-rendition
-                rendition.setType(RenditionType.STANDARD);
-            }
-            String templateString = renditionElement.getChildText("templateLanguage");
-            if (StringUtils.isEmpty(templateString)) {
-                throw new ThemeParsingException("rendition must contain a 'templateLanguage' element");
-            } else {
-                try {
-                    rendition.setTemplateLang(TemplateLanguage.valueOf(templateString.toUpperCase()));
-                } catch (IllegalArgumentException e) {
-                    throw new ThemeParsingException("Unknown templateLanguage value '" + templateString + "'");
-                }
-            }
-			rendition.setContentsFile(renditionElement.getChildText("contentsFile"));
-            if (StringUtils.isEmpty(rendition.getContentsFile())) {
-                throw new ThemeParsingException("Rendition must contain a 'contentsFile' element");
-            }
-			template.addTemplateRendition(rendition);
-		}
+        String navbar = element.getChildText("navbar");
+        if ("true".equalsIgnoreCase(navbar)) {
+            template.setNavbar(true);
+        }
 
-		String navbar = element.getChildText("navbar");
-		if ("true".equalsIgnoreCase(navbar)) {
-			template.setNavbar(true);
-		}
+        String hidden = element.getChildText("hidden");
+        if ("true".equalsIgnoreCase(hidden)) {
+            template.setHidden(true);
+        }
 
-		String hidden = element.getChildText("hidden");
-		if ("true".equalsIgnoreCase(hidden)) {
-			template.setHidden(true);
-		}
+        // validate template
+        if (StringUtils.isEmpty(template.getName())) {
+            throw new ThemeParsingException("templates must contain a 'name' element");
+        }
 
-		// validate template
-		if (StringUtils.isEmpty(template.getName())) {
-			throw new ThemeParsingException("templates must contain a 'name' element");
-		}
+        return template;
+    }
 
-		return template;
-	}
-    
-    
-    private ThemeMetadataTemplate elementToStylesheet(Element element) 
+    private ThemeMetadataTemplateRendition parseRendition(Element renditionElement)
             throws ThemeParsingException {
-        
+        ThemeMetadataTemplateRendition rendition = new ThemeMetadataTemplateRendition();
+        String renditionValue = renditionElement.getAttributeValue("type");
+        if (renditionValue != null) {
+            try {
+                rendition.setType(RenditionType.valueOf(renditionValue.toUpperCase()));
+            } catch (IllegalArgumentException e) {
+                throw new ThemeParsingException("Invalid rendition type " + renditionValue + " found.");
+            }
+        } else {
+            // default to standard if type not provided, as most templates are single-rendition
+            rendition.setType(RenditionType.STANDARD);
+        }
+        String templateString = renditionElement.getChildText("templateLanguage");
+        if (StringUtils.isEmpty(templateString)) {
+            throw new ThemeParsingException("rendition must contain a 'templateLanguage' element");
+        } else {
+            try {
+                rendition.setTemplateLang(TemplateLanguage.valueOf(templateString.toUpperCase()));
+            } catch (IllegalArgumentException e) {
+                throw new ThemeParsingException("Unknown templateLanguage value '" + templateString + "'");
+            }
+        }
+        rendition.setContentsFile(renditionElement.getChildText("contentsFile"));
+        if (StringUtils.isEmpty(rendition.getContentsFile())) {
+            throw new ThemeParsingException("Rendition must contain a 'contentsFile' element");
+        }
+        return rendition;
+    }
+
+    private ThemeMetadataTemplate elementToStylesheet(Element element)
+            throws ThemeParsingException {
         ThemeMetadataTemplate template = new ThemeMetadataTemplate();
-        
+
         template.setName(element.getChildText("name"));
         template.setDescription(element.getChildText("description"));
         template.setLink(element.getChildText("link"));
@@ -200,44 +208,18 @@ public class ThemeMetadataParser {
         // parsing rendition segment
         List<Element> renditionList = element.getChildren("rendition");
         for (Element renditionElement : renditionList) {
-            ThemeMetadataTemplateRendition rendition = new ThemeMetadataTemplateRendition();
-            String renditionValue = renditionElement.getAttributeValue("type");
-            if (renditionValue != null) {
-                try {
-                    rendition.setType(RenditionType.valueOf(renditionValue.toUpperCase()));
-                } catch (IllegalArgumentException e) {
-                    throw new ThemeParsingException("Invalid rendition type " + renditionValue + " found.");
-                }
-            } else {
-                // default to standard if type not provided, as most templates are single-rendition
-                rendition.setType(RenditionType.STANDARD);
-            }
-            String templateString = renditionElement.getChildText("templateLanguage");
-            if (StringUtils.isEmpty(templateString)) {
-                throw new ThemeParsingException("rendition must contain a 'templateLanguage' element");
-            } else {
-                try {
-                    rendition.setTemplateLang(TemplateLanguage.valueOf(templateString.toUpperCase()));
-                } catch (IllegalArgumentException e) {
-                    throw new ThemeParsingException("Unknown templateLanguage value '" + templateString + "'");
-                }
-            }
-            rendition.setContentsFile(renditionElement.getChildText("contentsFile"));
-            if (StringUtils.isEmpty(rendition.getContentsFile())) {
-                throw new ThemeParsingException("stylesheet must contain a 'contentsFile' element");
-            }
+            ThemeMetadataTemplateRendition rendition = parseRendition(renditionElement);
             template.addTemplateRendition(rendition);
         }
 
         // validate template
-        if(StringUtils.isEmpty(template.getName())) {
+        if (StringUtils.isEmpty(template.getName())) {
             throw new ThemeParsingException("stylesheet must contain a 'name' element");
         }
-        if(StringUtils.isEmpty(template.getLink())) {
+        if (StringUtils.isEmpty(template.getLink())) {
             throw new ThemeParsingException("stylesheet must contain a 'link' element");
         }
 
         return template;
     }
-    
 }
